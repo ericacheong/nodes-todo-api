883d38508cf903a54fca2530b1eab742547363df
Erica Cheong <erica.clw@gmail.com>
2018-04-12 11:56:36 +0800

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -38,7 +38,7 @@ UserSchema.methods.toJSON = function () {
     var userObject = user.toObject();
 
     return _.pick(userObject, ['_id', 'email']);
-}
+};
 
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
@@ -49,7 +49,18 @@ UserSchema.methods.generateAuthToken = function () {
     return user.save().then(() => {
         return token;
     });
-}
+};
+
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    });
+};
 
 UserSchema.statics.findByToken = function (token) {
     var User = this;
